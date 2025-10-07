69fde0ac1ea97c97bf91393a9ac94050c716b7f8
sanjayberi <berisanjay93@gmail.com>
2025-10-07 19:28:44 +0530

    Add Jest test and update scripts

--- test/app.test.js
+++ test/app.test.js
@@ -1,5 +1,12 @@
 const request = require("supertest");
 const app = require("../app");
+describe("GET /", () => {
+  it("should return 200 OK", async () => {
+    const res = await request(app).get("/");
+    expect(res.statusCode).toEqual(200);
+  });
+});
+
 
 describe("Account creation tests", () => {
   test("should fail if password does not match regex", async () => {
